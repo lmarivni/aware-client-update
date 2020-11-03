56c17a70d6acf33f411c7a47c83abfc191a4498a
Lynn <lmarivni.vannieuwenhove@ugent.be>
2020-11-03 10:05:07 +0100

    Methods in Provider are synchronized now

--- com.aware.plugin.smokeregistration/com.aware.plugin.smokeregistration/src/main/java/com/aware/plugin/smokeregistration/Provider.java
+++ com.aware.plugin.smokeregistration/com.aware.plugin.smokeregistration/src/main/java/com/aware/plugin/smokeregistration/Provider.java
@@ -151,7 +151,7 @@ public class Provider extends ContentProvider {
     }
 
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
+    public synchronized int delete(Uri uri, String selection, String[] selectionArgs) {
         initialiseDatabase();
 
         database.beginTransaction();
@@ -176,7 +176,7 @@ public class Provider extends ContentProvider {
 
     @Nullable
     @Override
-    public Uri insert(Uri uri, ContentValues new_values) {
+    public synchronized Uri insert(Uri uri, ContentValues new_values) {
 
         initialiseDatabase();
 
@@ -268,7 +268,7 @@ public class Provider extends ContentProvider {
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    public synchronized int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         initialiseDatabase();
 
         database.beginTransaction();
